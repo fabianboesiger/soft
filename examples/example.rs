f6407053929e6d856531a1ac9e71e984c1544e21
Fabian Bösiger <boesiger.fabian@outlook.com>
2021-09-21 14:41:41 +0200

    add example

--- examples/example.rs
+++ examples/example.rs
@@ -1,20 +1,10 @@
-# Soft Assertions
-
-This library provides soft, non-panicking assertions.
-Instead, the assertions provided by this crate returns a `Result`.
-To use this crate, simply replace `assert!(...)` by `soft::assert!()?`.
-
-## Example
-
-```
 fn main() {
     soft::assert!(true).unwrap();
     soft::assert!(false).unwrap_err();
 
     soft::assert_eq!(2, 2).unwrap();
     soft::assert_eq!(2, 3).unwrap_err();
-    
+
     soft::assert_ne!(2, 3).unwrap();
     soft::assert_ne!(2, 2).unwrap_err();
 }
-```
